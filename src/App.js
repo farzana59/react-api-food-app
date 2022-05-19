daf609895b6ec111b4ab3c894265cc9e945b8d44
Farzana <farzana_yasmin@outlook.com>
2022-05-19 19:24:53 +0600

    add project header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
 
-function App() {
+function App(){
+  var url= "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=c5a4a912&app_key=%20b2e1fea9c5c413dd25353996f5fa6cb4"
+
+ async function getRecipes(){
+    var result = await Axios.get(url);
+    console.log(result.data);
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">
+      <h1 onClick={getRecipes}> Food Recipe</h1>
     </div>
   );
+
 }
 
 export default App;
