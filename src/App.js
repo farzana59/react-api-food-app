34fbf96cfee3dec259a0f4148dd0ca5df21bbf14
Farzana <farzana_yasmin@outlook.com>
2022-05-21 22:10:37 +0600

    update recipe image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import "./App.css";
 import Axios from "axios";
+import {useState} from "react";
+import RecipeTile from "./RecipeTile";
 
 function App(){
-  var url= "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=c5a4a912&app_key=%20b2e1fea9c5c413dd25353996f5fa6cb4"
+  const [query, setQuery] = useState("");
+  const [recipes,setRecipes] = useState ([])
+
+  var url= `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id={Your_APP_ID}&app_key={Your_APP_KEY}`;
 
  async function getRecipes(){
     var result = await Axios.get(url);
+    setRecipes(result.data.hits);
     console.log(result.data);
   }
 
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  }
 
 
 
   return (
-    <div className= "App">
+    <div className= "app">
       <h1 onClick={getRecipes}> Food Recipe</h1>
+      <form className="app_searchForm" onSubmit={onSubmit}>
+        <input className="app_input"  type="text" placeholder="enter recipe name"
+        value={query} onChange={(e) => setQuery (e.target.value)} />
+
+        <input className="app_submit" type="submit" value="Search"/>
+      </form>
+      <div className="app_recipes">
+        {recipes.map((recipe, i) => {
+         return <RecipeTile key={i} recipe={recipe}/>;
+        
+        })}
+      </div>
     </div>
   );
 
